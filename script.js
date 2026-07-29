15d31f506a0ffb154fd9ebfc110571f52b48a3f7
Yash Makwana <yash.009675@gmail.com>
2026-07-29 20:00:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1988,3 +1988,50 @@ prevPhoto.click();
 });
 
 }
+/* ======================================================
+PHASE 7B.3
+3D PARALLAX
+====================================================== */
+
+const tiltCards = document.querySelectorAll(
+
+".card,.memory-content,.letter-card,.popup-card,.gift-card,.proposal-card"
+
+);
+
+tiltCards.forEach(card=>{
+
+card.addEventListener("mousemove",(e)=>{
+
+const rect = card.getBoundingClientRect();
+
+const x = e.clientX - rect.left;
+
+const y = e.clientY - rect.top;
+
+const rotateY =
+
+((x/rect.width)-0.5)*18;
+
+const rotateX =
+
+((rect.height/2-y)/rect.height)*18;
+
+card.style.transform=
+
+`perspective(900px)
+rotateX(${rotateX}deg)
+rotateY(${rotateY}deg)
+translateY(-8px)`;
+
+});
+
+card.addEventListener("mouseleave",()=>{
+
+card.style.transform=
+
+"perspective(900px) rotateX(0) rotateY(0)";
+
+});
+
+});
